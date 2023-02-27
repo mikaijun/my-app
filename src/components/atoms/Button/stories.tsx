045e2d89529a25b6977e27f9ad3d7a1e29186f7c
mikaijun <mikai@anyinc.jp>
2023-02-27 19:31:39 +0900

    Storybookにテストを導入

--- src/components/atoms/Button/stories.tsx
+++ src/components/atoms/Button/stories.tsx
@@ -1,3 +1,6 @@
+import { expect } from '@storybook/jest'
+import { within, userEvent } from '@storybook/testing-library'
+
 import Button from './index'
 
 import type { ComponentMeta, ComponentStoryObj } from '@storybook/react'
@@ -15,4 +18,19 @@ export const Primary: ComponentStoryObj<typeof Button> = {
     disabled: false,
     children: 'Button',
   },
+}
+
+// テストコード解説: https://blog.cybozu.io/entry/2022/08/29/110000
+Primary.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement)
+  console.log(canvas)
+
+  // <button> を取得し、クリックする
+  await userEvent.click(canvas.getByRole('button'))
+
+  // コンポーネントの文字がButtonかを確認
+  await expect(canvas.getByText('Button')).toBeInTheDocument()
+
+  // コンポーネントの文字がInputでないためエラーが出る
+  await expect(canvas.getByText('Input')).toBeInTheDocument()
 }
